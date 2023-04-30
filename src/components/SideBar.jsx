6fec60f92d2e283a79def9071261ca17542fd3c5
Shruthi <pshruthi2003@gmail.com>
2023-04-30 14:03:15 +0530

    First Commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import { Stack } from "@mui/material"
+import { categories } from "../utils/constants" 
+
+export const SideBar = () => {
+  return (
+    <Stack direction="row" sx={{overflowY:"auto",height:{sx:"auto",md:"95%"},flexDirection:{md:"column"}}}>
+
+    </Stack>
+  )
+}
